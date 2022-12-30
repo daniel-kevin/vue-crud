c9e2100dc6e2c00f602f21b7123f7890e204a93c
daniel-kevin <danielkevin179@gmail.com>
2022-12-30 09:13:13 +0800

    test

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/posts',[PostController::class,'index']);
-Route::post('/posts/store',[PostController::class,'store']);
-Route::get('/posts/{id?}',[PostController::class,'show']);
-Route::post('/posts/update/{id?}',[PostController::class,'update']);
-Route::delete('/posts/{id?}',[PostController::class,'destroy']);   
+// Route::get('/posts',[PostController::class,'index']);
+// Route::post('/posts/store',[PostController::class,'store']);
+// Route::get('/posts/{id?}',[PostController::class,'show']);
+// Route::post('/posts/update/{id?}',[PostController::class,'update']);
+// Route::delete('/posts/{id?}',[PostController::class,'destroy']);   
